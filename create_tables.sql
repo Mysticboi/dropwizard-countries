875440a3eba6b5a19332d381cd0234611b8372a4
Walid Oulderra <walidoulderra@hotmail.fr>
2022-02-07 20:55:15 +0100

    Create tables sql file

---
+++ create_tables.sql
@@ -0,0 +1,45 @@
+use countries;
+
+DROP TABLE IF EXISTS language;
+DROP TABLE IF EXISTS country_geo;
+DROP TABLE IF EXISTS currency;
+DROP TABLE IF EXISTS country;
+
+CREATE TABLE country (
+country_code varchar(5) PRIMARY KEY,
+capital varchar(50),
+official_name varchar(50),
+common_name varchar(50),
+flag varchar(200),
+population int,
+timezone varchar(20)
+);
+
+CREATE TABLE IF NOT EXISTS language(
+id int PRIMARY KEY AUTO_INCREMENT,
+language_code varchar(5),
+name varchar(50),
+country_code varchar(5),
+FOREIGN KEY(country_code) REFERENCES country(country_code)
+);
+
+
+CREATE TABLE country_geo(
+country_code varchar(5) PRIMARY KEY,
+area float,
+latitude float,
+longitude float,
+region varchar(30),
+subregion varchar(100),
+continent varchar(20),
+FOREIGN KEY(country_code) REFERENCES country(country_code)
+);
+
+CREATE TABLE currency(
+id int PRIMARY KEY AUTO_INCREMENT,
+currency_code varchar(10),
+name varchar(50),
+symbol varchar(5),
+country_code varchar(5),
+FOREIGN KEY(country_code) REFERENCES country(country_code)
+);
